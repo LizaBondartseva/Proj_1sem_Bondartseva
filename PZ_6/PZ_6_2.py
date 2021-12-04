9d49751afa9af1c57f4328fc3ceefa045a335a32
Student <LudaLiza1980@icloud.com>
2021-12-04 12:27:32 +0300

    1 commit

--- PZ_6/PZ_6_2.py
+++ PZ_6/PZ_6_2.py
@@ -2,10 +2,12 @@
 #Найти номер его первого локального минимума(локальный минимум - это элемент, который меньше любого из своих соседей).
 #Вариант №3
 
+N = int(input('Введите длину списка: '))
+A = []
+for i in range(N):
+    A.append(int(input('Введите число ' + str(i + 1) + ':')))
 
-n = int(input())
-arr = list(map(int, input().split()))
-for i in range(1, n-1):
-    if arr[i-1] > arr[i] < arr[i+1]:
-        print(i+1)
+for i in range (len(A) - 1):
+    if A[i] < A[i+1] and A[i] < A[i -1]:
+        print('Первый локальный минимум под номером ' + str(i+1))
         break
---
+++ PZ_7/PZ_7_2.py
@@ -0,0 +1,14 @@
+#Практическая работа №7
+#Вариант №3
+#Дана строка содержащая цифры и строчные латинские буквы. Если буквы в строке упорядочены по алфавиту, то вывести 0; в противном случае вывести номер первого символа строки, нарущающего алфавитный порядок.
+
+def f(a):
+    b = list(filter(lambda x: not x.isdigit(), sorted(list(a))))
+    d = list(filter(lambda x: not x.isdigit(), list(a)))
+    res = 0
+    for i in range(len(b)):
+        if b[i] != d[i]: res = d[i]; break
+    return res
+print(f('a4bc5d'))
+print(f('7ab6dct'))
+print(f('abcedf'))
